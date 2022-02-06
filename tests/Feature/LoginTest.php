d58ac58f7c54c1965ec6eb5d3a2e4eb9d058bf0f
Michał Wielopolski <77637937+wielopolski@users.noreply.github.com>
2022-02-06 23:45:20 +0100

    Test logownia

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -15,12 +15,21 @@ class LoginTest extends TestCase
         // Arrange
         // sprawdzenie logowania
 
-        $credentials=array(
+        $credentialsFalse=array(
             'email' => 'user@gmail.com',
             'password' => 'P@$$w0rd');
 
-
+        $credentialsTrue=array(
+            'email' => 'tester@gmail.com',
+            'password' => '654321');
+        // Act
+        // Wykonajmy zapytanie pod adres wpisu
+        $response_1 =Auth::attempt($credentialsFalse);
+        $response_2 =Auth::attempt($credentialsTrue);
         // Assert
-        $this->assertTrue(Auth::attempt($credentials));
+        // Sprawdźmy że w odpowiedzi znajduje się tytuł wpisu
+
+        $this->assertFalse($response_1);
+        $this->assertTrue($response_2);
     }
 }
